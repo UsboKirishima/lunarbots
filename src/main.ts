9684cc9899d22e9634e3e66d34dc010676515bb7
UsboKirishima <usbertibox@gmail.com>
2022-02-14 21:58:59 +0100

    feat(navbar)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import '../styles/style.css'
+//import 'bootstrap/dist/css/bootstrap.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle?.addEventListener("click", function() {
+  mainNav?.classList.toggle("active");
+});
